87bffb7da9b4553c21bedd282295bb59c46f4e6d
Goshan <goshan21rus@gmail.com>
2016-05-04 09:34:32 +0300

    corretion

--- lesson2/gburkov/src/Calculator.java
+++ lesson2/gburkov/src/Calculator.java
@@ -5,21 +5,17 @@ public class Calculator {
   
   public void add (double first, double second) {
     result = first + second;
-    System.out.println(result);
   }
    
   public void substruct (double first, double second) {
     result = first - second;
-    System.out.println(result);
   }
 
   public void div (double first, double second) {
     result = first / second;
-    System.out.println(result);
   }
 
   public void multiple (double first, double second) {
-    result = first * second;
-    System.out.println(result);
+    result = first * second;  
   }
 }
