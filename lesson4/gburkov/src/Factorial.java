4dfda6e88f6d0ba76ecf7747fc09ba42bfbaf803
Goshan <Goshan>
2016-05-09 16:11:33 +0300

    change Factorial

--- lesson4/gburkov/src/Factorial.java
+++ lesson4/gburkov/src/Factorial.java
@@ -2,7 +2,7 @@ package gburkov;
 
 public class Factorial {
 
-    public static int fact(int value) {
+    public int fact(int value) {
         if (value < 0) return -1;
         else if (value == 0) return 1;
         else {
