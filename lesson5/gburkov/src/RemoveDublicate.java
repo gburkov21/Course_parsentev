3a64c61d49abf8c8b3b1a3606ed95f50f0515943
Goshan <goshan21rus@gmail.com>
2016-05-11 17:44:45 +0300

    edit RemoveDublicate

--- lesson5/gburkov/src/RemoveDublicate.java
+++ lesson5/gburkov/src/RemoveDublicate.java
@@ -3,37 +3,20 @@ package gburkov;
 public class RemoveDublicate {
 
     public String[] removeDubl(String[] value) {
-        for (int i = 0; i < value.length; i++) {
-            for (int j = i + 1; j < value.length; j++) {
-                if (value[j].equals(value[i])) {
+        int index, length2;
+        String[] value2 = new String[value.length];
+        for (index = 0, length2 = 0; index < value.length; index++) {
+            for (int j = index + 1; j < value.length; j++) {
+                if (value[j].equals(value[index])) {
                     value[j] = "";
                 }
             }
-        }
-        int i, length2;
-        for (i = 0, length2 = 0; i < value.length; i++) {
-            if (!value[i].equals("")) {
+            if (!value[index].isEmpty()) {
+                value2[length2] = value[index];
                 length2++;
             }
         }
-        String[] value2 = new String[length2];
-        for (i = 0, length2 = 0; i < value.length; i++) {
-            if (!value[i].equals("")) {
-                value2[length2] = value[i];
-                length2++;
-            }
-        }
+        value2 = Arrays.copyOf(value2, length2);
         return value2;
     }
-
-//    public boolean isUnique (String value, String[] values) {
-//        boolean unique = true;
-//        for (String value1 : values) {
-//            if (value1.equals(value)) {
-//                unique = false;
-//            }
-//        }
-//        return unique;
-//    }
-
 }
