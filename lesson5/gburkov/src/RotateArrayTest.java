db91157e9f264170e8f2d65e6f6b947e945c98ad
Goshan <Goshan>
2016-05-15 15:40:19 +0300

    edit RotateArray

--- lesson5/gburkov/src/RotateArrayTest.java
+++ lesson5/gburkov/src/RotateArrayTest.java
@@ -2,17 +2,17 @@ package gburkov;
 
 public class RotateArrayTest {
     public static void main(String[] args) {
-        int[][] array = new int[3][3];
-        for (int i = 0; i < array.length; i++) {
-            for (int j = 0; j < array[i].length; j++) {
-                array[i][j] = (int) Math.round(Math.random() * 10);
+        int[][] values = new int[3][3];
+        for (int i = 0; i < values.length; i++) {
+            for (int j = 0; j < values[i].length; j++) {
+                values[i][j] = (int) Math.round(Math.random() * 10);
             }
         }
 
         RotateArray myArray = new RotateArray();
-        myArray.printArray(array);
+        myArray.printArray(values);
         System.out.println();
-        myArray.rotate(array);
-        myArray.printArray(array);
+        myArray.rotate(values);
+        myArray.printArray(values);
     }
 }
