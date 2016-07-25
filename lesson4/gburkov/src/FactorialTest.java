330025826fd09cf6745bc50ff5be7f600e6a7342
Georgy <goshan21rus@gmail.com>
2016-07-25 12:04:05 +0300

    delete

--- lesson4/gburkov/src/FactorialTest.java
+++
@@ -1,8 +0,0 @@
-package gburkov;
-
-public class FactorialTest {
-    public static void main(String[] args) {
-        Factorial factorial = new Factorial();
-        System.out.println(factorial.fact(5));
-    }
-}
